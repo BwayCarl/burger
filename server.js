05bb2209de6bc446d3127e4b7e238c698b77a054
Carlos <bwaycarl@gmail.com>
2020-10-01 12:02:35 -0400

    Dependencies and Port set up.

--- server.js
+++ server.js
@@ -1,1 +1,20 @@
 const express = require("express");
+const exphbs = require("express-handlebars");
+const mysql = require("mysql");
+
+const app = express();
+
+
+let PORT = process.env.PORT || 8080;
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+
+var routes = require("./controllers/burgers_controller.js");
+app.use(routes);
+
+app.listen(PORT, function() {
+    console.log("Listening on PORT ", PORT);
+  });
+  
