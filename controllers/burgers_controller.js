4d8c2a3c8d2ab2ae3d40dfcca3461ac7a95c97ae
Carlos <bwaycarl@gmail.com>
2020-10-01 12:00:14 -0400

    Routes.

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,9 +1,48 @@
 const express = require("express");
-const controller = require("./controllers/burgers_controller.js");
+const router = express.Router();
 
+var burger = require("../models/burger.js");
 
+router.get("/", function(req, res) {
+  burger.all(function(data) {
+    var hbsObject = {
+      burgers: data
+    };
+    console.log(hbsObject);
+    res.render("index", hbsObject);
+  });
+});
 
+router.post("/", function(req, res) {
+  burger.create([
+    "burger_name", "devoured"
+    ], 
+    [
+    req.body.burger_name, req.body.devoured
+    ], 
+  function() {
+    res.redirect("/");
+  });
+});
 
+router.put("/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
 
+  console.log("condition", condition);
 
-module.exports = controller;
+  burger.update({
+    devoured: req.body.devoured
+  }, condition, function() {
+    res.redirect("/");
+  });
+});
+
+router.delete("/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
+
+  burger.delete(condition, function() {
+    res.redirect("/");
+  });
+});
+
+module.exports = router;
