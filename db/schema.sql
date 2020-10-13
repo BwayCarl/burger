00422457eb39cc818028ca57dcbc7c595f517c42
Carlos <bwaycarl@gmail.com>
2020-10-13 00:33:47 -0400

    Update/adjust schema and seeds.

--- db/schema.sql
+++ db/schema.sql
@@ -1,12 +1,14 @@
+-- Drop database if it exists
 DROP DATABASE burgers_db;
 
+-- Create database
 CREATE DATABASE burgers_db;
 
 USE burgers_db;
 
 CREATE TABLE burgers (
     id INT AUTO_INCREMENT NOT NULL,
-    burger_name VARCHAR (255),
+    burger_name VARCHAR (255) NOT NULL,
     devoured BOOLEAN DEFAULT FALSE,
     PRIMARY KEY (id)
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,7 @@
-INSERT INTO burgers (burger_name, devoured) VALUES ('Texas BBQ Burger', FALSE);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Double Cheeseburger', FALSE);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Grilled Chicken Burger', FALSE);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burger', FALSE);
+INSERT INTO burgers 
+(burger_name, devoured) 
+VALUES 
+('Texas BBQ Burger', FALSE),
+('Bacon Double Cheeseburger', FALSE),
+('Grilled Chicken Burger', FALSE),
+('Veggie Burger', FALSE);
