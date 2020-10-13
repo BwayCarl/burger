93cc23bb54e95e613d443abe8035b2223ed576ff
Carlos <bwaycarl@gmail.com>
2020-10-13 00:15:38 -0400

    Deleted personal password.

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
-  password: "NewYorkMets!1986", //Insert your MySQL password here.
+  password: "", //Insert your MySQL password here.
   database: "burgers_db"
 });
 
