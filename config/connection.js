5cbdeaf26cf9d2a1b486b63cfda657744629cbaf
Carlos <bwaycarl@gmail.com>
2020-10-21 13:53:57 -0400

    Changed connection to include JAWSDB, removed MySQL password.

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,19 @@
 const mysql = require("mysql");
 
-var connection = mysql.createConnection({
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
+
+} else {
+  connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
-  password: "NewYorkMets!1986", //Insert your MySQL password here.
+  password: "", //Insert your MySQL password here.
   database: "burgers_db"
 });
+}
 
-connection.connect(function(err) {
-  if (err) {
-    console.error("Error connecting: " + err.stack);
-    return;
-  }
-  console.log("Connected as ID " + connection.threadId);
-});
+connection.connect();
 
 module.exports = connection;
